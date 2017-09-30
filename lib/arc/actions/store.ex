28936e285922cef0409e793f7e23960ac2282f6f
Igor Belo <igorcoura@gmail.com>
2017-09-30 19:58:17 +0200

    work in progress with store bit

--- lib/arc/actions/store.ex
+++ lib/arc/actions/store.ex
@@ -1,65 +1,73 @@
 defmodule Arc.Actions.Store do
   defmacro __using__(_) do
     quote do
-      def store(args), do: Arc.Actions.Store.store(__MODULE__, args)
+      def store(source_file, options \\ []), do: Arc.Actions.Store.store(source_file, options ++ [definition: __MODULE__])
     end
   end
 
-  def store(definition, {file, scope}) when is_binary(file) or is_map(file) do
-    put(definition, {Arc.File.new(file), scope})
-  end
+  def store(source_file, options) do
+    definition = options[:definition]
 
-  def store(definition, filepath) when is_binary(filepath) or is_map(filepath) do
-    store(definition, {filepath, nil})
-  end
-
-  #
-  # Private
-  #
-
-  defp put(_definition, { error = {:error, _msg}, _scope}), do: error
-  defp put(definition, {%Arc.File{}=file, scope}) do
-    case definition.validate({file, scope}) do
-      true -> put_versions(definition, {file, scope})
-      _    -> {:error, :invalid_file}
-    end
-  end
-
-  defp put_versions(definition, {file, scope}) do
-    if definition.async do
-      definition.__versions
-      |> Enum.map(fn(r)    -> async_put_version(definition, r, {file, scope}) end)
-      |> Enum.map(fn(task) -> Task.await(task, version_timeout()) end)
-      |> handle_responses(file.file_name)
+    with :ok <- definition.validate(source_file, options),
+        {:ok, tmp_file} <- store_tmp(source_file, options),
+        {:ok, files} <- convert(tmp_file, options),
+        {:ok, all_files} <- store_all_files(files, options) do
+      {:ok, all_files}
     else
-      definition.__versions
-      |> Enum.map(fn(version) -> put_version(definition, version, {file, scope}) end)
-      |> handle_responses(file.file_name)
+      error -> error
     end
   end
 
-  defp handle_responses(responses, filename) do
-    errors = Enum.filter(responses, fn(resp) -> elem(resp, 0) == :error end) |> Enum.map(fn(err) -> elem(err, 1) end)
-    if Enum.empty?(errors), do: {:ok, filename}, else: {:error, errors}
+  defp read_file(source_file) do
+    File.read(source_file)
   end
 
-  defp version_timeout do
-    Application.get_env(:arc, :version_timeout) || 15_000
+  defp store_tmp(source_file, options) do
+    case read_file(source_file) do
+      {:ok, file_content} ->
+        tmp_file = System.tmp_dir <> uuid <> Path.extname(source_file)
+        File.write(tmp_file, file_content)
+        {:ok, tmp_file}
+      error -> error
+    end
   end
 
-  defp async_put_version(definition, version, {file, scope}) do
-    Task.async(fn ->
-      put_version(definition, version, {file, scope})
+  defp convert(file, [dimensions: dimensions]) when dimensions do
+    converted_files = Enum.map(dimensions, fn(dimension) ->
+      System.cmd("convert", [file, "-resize", dimension, "#{file}-#{dimension}"])
+      "#{file}-#{dimension}"
     end)
+    {:ok, [file] ++ converted_files}
   end
 
-  defp put_version(definition, version, {file, scope}) do
-    case Arc.Processor.process(definition, version, {file, scope}) do
-      {:error, error} -> {:error, error}
-      {:ok, file} ->
-        file_name = Arc.Definition.Versioning.resolve_file_name(definition, version, {file, scope})
-        file      = %Arc.File{file | file_name: file_name}
-        definition.__storage.put(definition, version, {file, scope})
-    end
+  defp convert(file, _options) do
+    {:ok, [file]}
+  end
+
+  defp store_all_files(files, options) do
+    files = Enum.map(files, fn(file) ->
+      destination_path = destination_path(file, options)
+      destination_file = destination_file(destination_path, file, options)
+      case File.cp(file, destination_file) do
+        :ok -> Path.expand(destination_file)
+        error -> error
+      end
+    end)
+    {:ok, files}
+  end
+
+  defp uuid do
+    length = 32
+    :crypto.strong_rand_bytes(length) |> Base.encode64 |> binary_part(0, length)
+  end
+
+  defp destination_path(file, options) do
+    path = options[:definition].storage_dir(file, options)
+    if !File.exists?(path), do: File.mkdir_p(path)
+    path
+  end
+
+  defp destination_file(path, file, options) do
+    Path.join(path, Path.basename(file))
   end
 end
